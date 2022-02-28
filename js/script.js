8e201c72b10fb5112d6b20ca7d0cea51422b7eb1
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-02-28 14:22:04 -0500

    added query selectors for hamburger

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const navToggle = document.querySelector('#nav-toggle');
+const barTop = document.querySelector('.bar-top');
+const barBottom = document.querySelector('.bar-bottom');
 
-navToggle.addEventListener('click', () => {
+navToggle.addEventListener('click', transform)
 
-})
+function transform () {
+  console.log('test');
+  barTop.classList.toggle('change');
+  barBottom.classList.toggle('change');
+}
