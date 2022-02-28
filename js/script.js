b62c877a658533619fedc8af454cf882750bb4a8
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-02-28 16:58:56 -0500

    Add query Selectors & Functions to expand mobile menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
+const topNav = document.querySelector('.top-nav');
 const navToggle = document.querySelector('#nav-toggle');
 const barTop = document.querySelector('.bar-top');
 const barBottom = document.querySelector('.bar-bottom');
+const barMiddle = document.querySelector('.bar-middle');
+const navLinks = document.querySelectorAll('.nav-link');
+const navLink = document.querySelector('.nav-link');
+const logo = document.querySelector('.logo');
 
-navToggle.addEventListener('click', transform)
 
-function transform () {
+
+navToggle.addEventListener('click', () => {
+  transform();
+  expand();
+})
+
+function transform() {
   console.log('test');
   barTop.classList.toggle('change');
   barBottom.classList.toggle('change');
+  barMiddle.classList.toggle('hidden');
+}
+
+function expand() {
+  topNav.classList.add('fill-height');
+  navLinks.forEach(link => link.classList.add('display-block'));
 }
