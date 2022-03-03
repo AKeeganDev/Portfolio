422346f3f1252193cc63ccc29f5b90aebffb63cf
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-03-03 12:14:11 -0500

    fix linter errors

--- js/script.js
+++ js/script.js
@@ -377,23 +377,6 @@ const form = document.getElementsByTagName('form')[0];
 const email = document.getElementById('mail');
 const emailError = document.querySelector('#mail + span.error');
 
-email.addEventListener('input', function (event) {
-
-  if (email.validity.valid) {
-    emailError.textContent = '';
-    emailError.className = 'error';
-  } else {
-    showError();
-  }
-});
-
-form.addEventListener('submit', function (event) {
-  if (!email.validity.valid) {
-    showError();
-    event.preventDefault();
-  }
-});
-
 function showError() {
   if (email.validity.valueMissing) {
     emailError.textContent = 'You need to enter an e-mail address.';
@@ -404,3 +387,19 @@ function showError() {
   }
   emailError.className = 'error active';
 }
+
+email.addEventListener('input', () => {
+  if (email.validity.valid) {
+    emailError.textContent = '';
+    emailError.className = 'error';
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
